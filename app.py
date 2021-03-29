cc0195415d058b6bd85a08b2fd3daad876fe296b
DanielBravoFlores <d.bravo.flores@gmail.com>
2021-03-29 09:51:24 -0300

    versión inicial

---
+++ app.py
@@ -0,0 +1,48 @@
+import sys
+import logging.config
+import json
+
+from send_mail import MailSender
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+@app.route("/sendmail", methods=["POST"])
+def test_functions():
+    plaintext = ""
+
+    asunto = "Confirmación hora"
+    sender = "info Eva"
+
+    try:
+        request_body = json.loads(request.data)
+        print("REQ: "+str(request_body))
+        result = {
+                    "option": "EMAIL",
+                    "visibleContext":request_body["visibleContext"],
+                    "openContext":request_body["openContext"],
+                    "hiddenContext":request_body["hiddenContext"]        
+        }
+        info = request_body["info"]
+
+        data = {
+            'sessionCode': info["sessionCode"],
+            'name': request_body["text"]
+        }
+
+        ourmailsender = MailSender('info.eva.everis@gmail.com', 'sbjhvaqwgjcuvwsz', ('smtp.gmail.com', 587))
+        ourmailsender.set_message(plaintext, asunto, sender)
+        ourmailsender.set_recipients([request_body["text"]])        
+        ourmailsender.connect()
+        ourmailsender.send_all()
+    except:
+        logging.exception("Unexpected error parsing payload: %s ", sys.exc_info()[0])
+        result = "Error..."
+
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    #start_logging_conf()    
+    app.run(debug=True, port=8002)
+    #test_functions()
